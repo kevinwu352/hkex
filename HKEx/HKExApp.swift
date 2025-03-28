29dd26f37bd5499a094564916a10d619ac3f1921
kevin <kevinwu352@gmail.com>
2025-03-28 15:49:21 +0800

    init

---
+++ HKEx/HKExApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKExApp.swift
+//  HKEx
+//
+//  Created by Kevin Wu on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HKExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
